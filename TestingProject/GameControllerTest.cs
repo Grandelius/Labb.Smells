6a25b46acd91ef08c2a631f5453b5969cde90768
Grandelius <jonatan.grandelius@iths.se>
2023-08-28 17:07:51 +0200

    Implemented test for createUserName method

--- TestingProject/GameControllerTest.cs
+++ TestingProject/GameControllerTest.cs
@@ -6,6 +6,12 @@ namespace Labb.Smells.Classes.Tests
     [TestClass]
     public class GameControllerTest
     {
+        public GameController gamecontroller;
+
+        public GameControllerTest()
+        {
+            this.gamecontroller = new GameController(new Mock<IUI>().Object, new Mock<IPlayerData>().Object, new Mock<IRandomNumberGenerator>().Object);
+        }
   
         [TestMethod]
         public void TestCreateTargetNumbers()
@@ -20,13 +26,33 @@ namespace Labb.Smells.Classes.Tests
                 .Returns(3)
                 .Returns(4);
 
-            var gameController = new GameController(new Mock<IUI>().Object, new Mock<IPlayerData>().Object, new Mock<IRandomNumberGenerator>().Object);
+            //var gameController = new GameController(new Mock<IUI>().Object, new Mock<IPlayerData>().Object, new Mock<IRandomNumberGenerator>().Object);
 
             // Act
-            string target = gameController.CreateTargetNumber();
+            string target = gamecontroller.CreateTargetNumber();
 
             // Assert
             Assert.AreEqual("0123", target);
+        }
+
+        [TestMethod]
+        public void CreateUserName()
+        {
+            var ioMock = new Mock<IUI>();
+            ioMock.Setup(io => io.GetInput()).Returns("TestUser");
+
+            gamecontroller.io = ioMock.Object;
+
+            //var playerDataMock = new Mock<IPlayerData>();
+            //var randomNumberGeneratorMock = new Mock<IRandomNumberGenerator>();
+
+            //var controller = new GameController(ioMock.Object, playerDataMock.Object, randomNumberGeneratorMock.Object);
+
+            string userName = gamecontroller.CreateUserName(); /// INSPEKTERA IMORGON
+
+            Assert.AreEqual("TestUser", userName);
+            ioMock.Verify(io => io.Print("Enter your user name:"), Times.Once);
+            ioMock.Verify(io => io.GetInput(), Times.Once);
         }
     }
 }
